1ded51292fefbc9ca34790c14828aae92205e149
ivannissimrch <ivannissimrch@gmail.com>
2024-05-12 21:11:37 -0400

    Add basic functionality to fetch weather data from API

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,41 @@
+import { getWeatherInfo } from "./getWeatherInfo";
+import "./style.css";
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <main>
-  
+  <form >
+  <input type='text'  id='enteredUserInput' placeholder='enter city name'>  
+ 
+  </form>
+  <section id ='results'> 
+  <p id='displayWeather'></p> 
+  </section>
   </main>
 `;
 
-// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+let cityNameInput: HTMLInputElement | null =
+  document.querySelector("#enteredUserInput");
+let displayWeatherInfo: HTMLParagraphElement =
+  document.getElementById("displayWeather");
+let receivedweatherData: { city: string; weather: string; temp: string } | null;
+
+cityNameInput?.addEventListener(
+  "keydown",
+  async (event: KeyboardEvent): Promise<void> => {
+    if (event.key === "Enter") {
+      try {
+        event.preventDefault();
+        let cityEntered = cityNameInput.value;
+        cityNameInput.value = "";
+        receivedweatherData = await getWeatherInfo(cityEntered);
+
+        if (receivedweatherData) {
+          const { city, temp, weather } = receivedweatherData;
+          displayWeatherInfo.innerHTML = `${city} Temperature: ${temp} Weather Conditions: ${weather}`;
+        }
+      } catch (error) {
+        displayWeatherInfo.innerHTML = `City not found, please try again`;
+      }
+    }
+  }
+);
